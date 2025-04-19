def42db6c0a705c0e29e71f63b5ff88727501a8f
jared9535 <jared9535@aol.com>
2025-04-19 13:45:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-const PORT = process.env.PORT || 3000; // <--- THIS IS IMPORTANT!
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable CORS for all requests
 app.use(cors());
 
 // Dummy Amazon Orders Endpoint
